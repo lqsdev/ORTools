7fd6314c95af601cfef7ecf8b0f2ba772beab8a8
Zenologo <ppc2006.yan@gmail.com>
2014-01-08 20:53:12 +0800

    test logger

--- Main.java
+++ Main.java
@@ -1,25 +1,30 @@
 package fodt;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.text.DecimalFormat;
+import java.util.Formatter;
 import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.log4j.Logger;
 import org.dom4j.DocumentException;
 import org.xml.sax.SAXException;
 
 public class Main {
-
+	
+	private static Logger logger = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) throws DocumentException, ParserConfigurationException, SAXException, IOException {
+		
 		TagMap mapConfig = new TagMap();
 		Map<String, List<tag>> test = mapConfig.readConfig();
 		
 		
 		FodtReader fodtReader = new FodtReader(test);
 		if (!fodtReader.readFodtFile()){
-			System.out.println("Error: Reader fodt file is failed!");
+			logger.error("Reader fodt file is failed!");
 		}
 		
 		
